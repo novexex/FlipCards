ddd611c7f35f668798cc42043deb8c34ee824695
novexex <104654115+novexex@users.noreply.github.com>
2022-11-04 18:09:01 +0300

    added assert on init

--- FlipCards/ConcentrationGame.swift
+++ FlipCards/ConcentrationGame.swift
@@ -24,9 +24,9 @@ class ConcentrationGame {
     private var prevButtonIndex: Int?
     
     private func chooseCard(at index: Int) {
-        if !cards[index].isMatched { //проверяем, не нажал ли пользователь на уже угаданную пару карт
-            if let matchingIndex = indexOfFaceUpCard, matchingIndex != index { //нам в функцию приходит индекс и мы проверяем, сейчас есть ли какая то перевёрнутая карта, если да, то не соответствует ли она той, что к нам пришла
-                if cards[matchingIndex].identifier == cards[index].identifier { //если идентификаторы совпадают (т.е. смайлики одинаковые), то мэтч
+        if !cards[index].isMatched {
+            if let matchingIndex = indexOfFaceUpCard, matchingIndex != index {
+                if cards[matchingIndex].identifier == cards[index].identifier {
                     cards[matchingIndex].isMatched = true
                     cards[index].isMatched = true
                     hideAfterMatchIndex = index
@@ -50,6 +50,7 @@ class ConcentrationGame {
     }
     
     init(numsOfPairsOfCards: Int) {
+        assert(numsOfPairsOfCards > 0, "init(numsOfPairsOfCards is less then zero)")
         for _ in 1...numsOfPairsOfCards {
             let card = Card()
             cards += [card, card]
